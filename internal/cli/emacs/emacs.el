173caa2131221d30bc3c2843c49cd34b2d2dd089
initbar <initbar@protonmail.ch>
2018-01-25 00:35:20 -0500

    unbind termiantor keys

--- internal/cli/emacs/emacs.el
+++ internal/cli/emacs/emacs.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/custom")
 (load-library "packages")
 (load-library "packages-config")
@@ -6,3 +13,17 @@
 (load-library "aliases")
 (load-library "interface")
 (load-library "syntax")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (toml-mode swoop rust-mode php-mode paradox linum-relative json-mode go-mode dockerfile-mode auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
