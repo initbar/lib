44fdf964b6b597142e98ba20993e0f2029dc06d0
initbar <initbar@protonmail.ch>
2018-02-16 15:07:13 +0000

    do not highlight line

--- internal/cli/emacs/custom/interface.el
+++ internal/cli/emacs/custom/interface.el
@@ -14,8 +14,8 @@
 (add-hook 'prog-mode-hook 'whitespace-mode)
 
 ; highlight current line
-(global-hl-line-mode 1)
-(set-face-attribute 'region nil :background "#222" :foreground "#ff0000")
+;(global-hl-line-mode 1)
+;(set-face-attribute 'region nil :background "#222" :foreground "#ff0000")
 
 ; match parenthesis
 (show-paren-mode 1)
