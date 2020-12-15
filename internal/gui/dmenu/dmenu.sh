bf156638b093f9125161f52f66304e0cd74ce711
initbar <initbar@protonmail.ch>
2020-12-15 18:01:19 -0500

    scale font size to 4k display

--- internal/gui/dmenu/dmenu.sh
+++ internal/gui/dmenu/dmenu.sh
@@ -3,5 +3,5 @@ nf="#fafafa"
 nb="#0a0a0a"
 sf="#fafafa"
 sb="#d70000"
-fn="Terminus (TTF)-8px3P3Ppx"
+fn="Terminus (TTF)-9px3P3Ppx"
 exe="$(dmenu_path | dmenu_run -b -i -nb "$nb" -nf "$nf" -sb "$sb" -sf "$sf" -fn "$fn")" && { eval "exec $exe";} && { exit ${?};}
