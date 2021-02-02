f2d19424f1812b8375c5a6a31bb76718a96d39f8
Herbert Shin <h@init.bar>
2021-02-02 15:15:31 -0500

    emacs: clean up configurations

--- internal/cli/emacs/emacs.el
+++ internal/cli/emacs/emacs.el
@@ -18,13 +18,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (doom-peacock)))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes nil)
  '(custom-safe-themes
    (quote
-    ("151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "554b7f0439155d6eb648d4837ef03902f51124cacee021217e76f39e9dd314c2" "53d1bb57dadafbdebb5fbd1a57c2d53d2b4db617f3e0e05849e78a4f78df3a1b" "7666b079fc1493b74c1f0c5e6857f3cf0389696f2d9b8791c892c696ab4a9b64" "2a1b4531f353ec68f2afd51b396375ac2547c078d035f51242ba907ad8ca19da" "a866134130e4393c0cad0b4f1a5b0dd580584d9cf921617eee3fd54b6f09ac37" "4e21fb654406f11ab2a628c47c1cbe53bab645d32f2c807ee2295436f09103c6" "b2e1acc5cba1ea79e9b7fe18f09e8f69bee15c074910bc6b42f5728e7c5cdf88" "a142def5bab9c1d1964eb2bf7506e790fa09df44aa9f3e41abd198f00cd6577e" "b5ecb5523d1a1e119dfed036e7921b4ba00ef95ac408b51d0cd1ca74870aeb14" default)))
- '(package-selected-packages
-   (quote
-    (mark-multiple yaml-mode py-autopep8 sr-speedbar toml-mode swoop rust-mode php-mode paradox linum-relative json-mode go-mode dockerfile-mode auto-complete))))
+    ("3d4df186126c347e002c8366d32016948068d2e9198c496093a96775cc3b3eaa" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
