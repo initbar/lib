1aaf5dcf73456cdae31839ea2c83e84236cb2e13
Herbert Shin <h@init.bar>
2021-07-12 16:10:52 -0400

    setup: comment vagrant

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,11 +18,11 @@
   }
 
   # vagrant
-  [ -n "$(which vagrant)" ] || {
-    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-    sudo apt install -y --no-install-recommends vagrant
-  }
+  # [ -n "$(which vagrant)" ] || {
+  #   curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+  #   sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  #   sudo apt install -y --no-install-recommends vagrant
+  # }
 )
 
 {
