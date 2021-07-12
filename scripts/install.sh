62791f805c5498ec210ecaecb2e859affd1c8890
Herbert Shin <h@init.bar>
2021-07-12 15:26:17 -0400

    setup: add gcloud

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,6 +10,13 @@
     ln -vsf ~/.lib/internal/etc/dnscrypt-proxy.toml dnscrypt-proxy.toml
   }
 
+  # gcloud
+  [ -n $(which gcloud) ] || {
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    sudo apt-get update && sudo apt-get install -y --no-install-recommends google-cloud-sdk
+  }
+
   # vagrant
   [ -n $(which vagrant) ] || {
     curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
