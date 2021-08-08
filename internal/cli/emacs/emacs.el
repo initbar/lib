4b678179026ebe3dd37db4b77d15e0f91b72306b
Herbert Shin <h@init.bar>
2021-08-08 18:52:54 -0400

    emacs: clean up temporary config

--- internal/cli/emacs/emacs.el
+++ internal/cli/emacs/emacs.el
@@ -13,23 +13,3 @@
 (load-library "packages-config")
 (load-library "shortcuts")
 (load-library "syntax")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes nil)
- '(custom-safe-themes
-   '("3d4df186126c347e002c8366d32016948068d2e9198c496093a96775cc3b3eaa" default))
- '(package-selected-packages
-   '(yaml-mode toml-mode terraform-mode swoop sass-mode rust-mode protobuf-mode linum-relative json-mode go-mode dockerfile-mode cython-mode auto-complete tabbar session pod-mode muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode folding eproject diminish csv-mode browse-kill-ring boxquote bm bar-cursor apache-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
