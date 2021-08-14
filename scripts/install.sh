52a33ab2edaced9ecb61ca7c8a99f25d3b00534d
Herbert Shin <h@init.bar>
2021-08-14 16:38:11 -0400

    scripts: clean up and add more symlink tests

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,40 +2,28 @@
 
 (
   # dnscrypt
-  rm -vrf ~/.dnscrypt && mkdir -p ~/.dnscrypt
-  curl -L --output - https://github.com/DNSCrypt/dnscrypt-proxy/releases/download/2.0.45/dnscrypt-proxy-linux_x86_64-2.0.45.tar.gz | tar -xz -C ~/.dnscrypt
-  cd ~/.dnscrypt && {
-    mv -v linux-x86_64/* .
-    rm -vrf linux-x86_64
-    ln -vsf ~/.lib/internal/etc/dnscrypt-proxy.toml dnscrypt-proxy.toml
+  {
+    rm -vrf ~/.dnscrypt/ && mkdir -p ~/.dnscrypt/
+    curl -L --output - https://github.com/DNSCrypt/dnscrypt-proxy/releases/download/2.1.0/dnscrypt-proxy-linux_x86_64-2.1.0.tar.gz | tar -xz -C ~/.dnscrypt
+    cd ~/.dnscrypt/ && {
+      ln -vsf ~/.lib/internal/etc/dnscrypt-proxy.toml dnscrypt-proxy.toml
+      mv -v   linux-x86_64/* .
+      rm -vrf linux-x86_64/
+    }
   }
-
-  # gcloud
-  # [ -n "$(which gcloud)" ] || {
-  #   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-  #   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-  #   sudo apt-get update && sudo apt-get install -y --no-install-recommends google-cloud-sdk
-  # }
-
-  # vagrant
-  # [ -n "$(which vagrant)" ] || {
-  #   curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-  #   sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  #   sudo apt install -y --no-install-recommends vagrant
-  # }
 )
 
 {
-  [ -d ~/.lib ] || git clone https://github.com/initbar/dotfiles.git ~/.lib
+  [ -d ~/.lib/ ] || git clone https://github.com/initbar/dotfiles.git ~/.lib/
 
-  cd ~/.lib && {
+  cd ~/.lib/ && {
     rm -vrf submodules/ && git submodule update --init --recursive
 
     #
     # external
     #
     (
-      cd external && {
+      cd external/ && {
         for i in */**.gz; do tar -xvf $i -C $(dirname $i); done
 
         # cursors
@@ -43,7 +31,7 @@
         ln -vsf ~/.lib/external/cursor ~/.icons
 
         # fonts
-        mkdir -p ~/.local/share
+        mkdir -p ~/.local/share/
         test -L ~/.local/share/fonts && unlink ~/.local/share/fonts
         ln -vsf ~/.lib/external/fonts ~/.local/share/fonts
 
@@ -57,33 +45,51 @@
     # internal
     #
     (
-      cd internal && {
+      cd internal/ && {
 
         (
-          cd cli && {
+          cd cli/ && {
 
             # bash
+            test -L ~/.bashrc && unlink ~/.bashrc
             ln -vsf ~/.lib/internal/cli/bash/bashrc ~/.bashrc
 
-            # emacs*
-            test -L ~/.emacs.d && unlink ~/.emacs.d
-            ln -vsf ~/.lib/internal/cli/emacs.d ~/.emacs.d
-            ln -vsf ~/.lib/internal/cli/emacs/emacs.el ~/.emacs
+            # emacs
+            {
+              test -L ~/.emacs.d && unlink ~/.emacs.d
+              ln -vsf ~/.lib/internal/cli/emacs.d ~/.emacs.d
+
+              test -L ~/.emacs && unlink ~/.emacs
+              ln -vsf ~/.lib/internal/cli/emacs/emacs.el ~/.emacs
+            }
+
+            # docker
+            mkdir -vp ~/.docker
+            test -L ~/.docker/config.json && unlink ~/.docker/config.json
+            ln -vsf ~/.lib/internal/docker/config.json ~/.docker/config.json
 
             # nano
+            test -L ~/.nanorc && unlink ~/.nanorc
             ln -vsf ~/.lib/internal/cli/nano/nanorc ~/.nanorc
 
             # zsh
-            ln -vsf ~/.lib/submodules/zsh ~/.zsh.d
-            ln -vsf ~/.lib/internal/cli/zsh/zshrc.sh ~/.zshrc
-            ln -vsf ~/.lib/internal/cli/zsh/zsh_aliases.sh ~/.zsh_aliases
+            {
+              test -L ~/.zsh.d && unlink ~/.zsh.d
+              ln -vsf ~/.lib/submodules/zsh ~/.zsh.d
+
+              test -L ~/.zshrc && unlink ~/.zshrc
+              ln -vsf ~/.lib/internal/cli/zsh/zshrc.sh ~/.zshrc
+
+              test -L ~/.zsh_aliases && unlink ~/.zsh_aliases
+              ln -vsf ~/.lib/internal/cli/zsh/zsh_aliases.sh ~/.zsh_aliases
+            }
           }
         )
 
         (
-          cd etc && {
+          cd etc/ && {
 
-            # hosts.{allow, deny}
+            # hosts.*
             cat hosts.allow | sudo tee /etc/hosts.allow
             cat hosts.deny | sudo tee /etc/hosts.deny
 
@@ -110,7 +116,7 @@
         )
 
         (
-          cd gui && {
+          cd gui/ && {
 
             # terminator
             mkdir -vp ~/.config
