464d5c0576aaaf838629786393c53ff255949049
Herbert Shin <h@init.bar>
2021-11-06 18:49:29 -0400

    emacs: update emacs autogen

--- internal/cli/emacs/emacs.el
+++ internal/cli/emacs/emacs.el
@@ -19,8 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (yaml-mode toml-mode terraform-mode swoop sass-mode rust-mode protobuf-mode markdown-mode linum-relative json-mode go-mode dockerfile-mode cython-mode auto-complete))))
+   '(yaml-mode toml-mode terraform-mode swoop sass-mode rust-mode protobuf-mode markdown-mode linum-relative json-mode go-mode dockerfile-mode cython-mode auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
