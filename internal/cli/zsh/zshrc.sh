96a23f4368ce81f61b4bda4761758e893d7868d9
Herbert Shin <h@init.bar>
2021-11-06 21:20:01 -0400

    zsh: update rcs

--- internal/cli/zsh/zshrc.sh
+++ internal/cli/zsh/zshrc.sh
@@ -2,36 +2,22 @@
 
 [[ -o login && -o interactive ]] && {
 
-  #
-  # Zsh plugins & configurations
-  #
-
+  # oh-my-zsh
   {
-    export _ZSH_OH_MY_ZSH="${HOME}/.zsh.d/oh-my-zsh"
-    export _ZSH_PLUGINS="${HOME}/.zsh.d/plugins"
-    export ZSH=$_ZSH_OH_MY_ZSH
-
     export DISABLE_AUTO_UPDATE=true
     export DISABLE_MAGIC_FUNCTIONS=true
     export DISABLE_UPDATE_PROMPT=true
     export ZSH_THEME=gentoo
 
+    export _ZSH_OH_MY_ZSH="${HOME}/.zsh.d/oh-my-zsh"
+    export _ZSH_PLUGINS="${HOME}/.zsh.d/plugins"
+    export ZSH="${_ZSH_OH_MY_ZSH}"
+
     # oh-my-zsh
     . "${_ZSH_OH_MY_ZSH}/oh-my-zsh.sh"
-
-    # plugins
     . "${_ZSH_PLUGINS}/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
     . "${_ZSH_PLUGINS}/zsh-completions/zsh-completions.plugin.zsh"
     . "${_ZSH_PLUGINS}/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
-
-    # rcs
-    [ -f /etc/zsh_command_not_found ] && {
-      . /etc/zsh_command_not_found
-    }
-
-    [ -f "${HOME}/.zsh_aliases" ] && {
-      . "${HOME}/.zsh_aliases"
-    }
 
     setopt AUTOCD             # type the name of a directory to switch
     setopt RCQUOTES           # elegant nested quotations
@@ -46,53 +32,66 @@
     setopt NUMERIC_GLOB_SORT  # numeric sort the globs
   }
 
-  export GREP_COLOR="97;45"
-  export LANG="C.UTF-8"
-  export LANGUAGE="C.UTF-8"
-  export LC_ALL="C.UTF-8"
-  export LC_CTYPE="C.UTF-8"
-  export LESS_TERMCAP_mb=$(printf '\e[01;31m')
-  export LESS_TERMCAP_md=$(printf '\e[01;35m')
-  export LESS_TERMCAP_me=$(printf '\e[0m')
-  export LESS_TERMCAP_se=$(printf '\e[0m')
-  export LESS_TERMCAP_so=$(printf '\e[01;33m')
-  export LESS_TERMCAP_ue=$(printf '\e[0m')
-  export LESS_TERMCAP_us=$(printf '\e[04;36m')
-  export LS_COLORS="${LS_COLORS}:di=1;91:"
-  export TERM="xterm-256color"
+  # envs
+  {
+    export GREP_COLOR="97;45"
+    export LANG="C.UTF-8"
+    export LANGUAGE="C.UTF-8"
+    export LC_ALL="C.UTF-8"
+    export LC_CTYPE="C.UTF-8"
+    export LESS_TERMCAP_mb=$(printf '\e[01;31m')
+    export LESS_TERMCAP_md=$(printf '\e[01;35m')
+    export LESS_TERMCAP_me=$(printf '\e[0m')
+    export LESS_TERMCAP_se=$(printf '\e[0m')
+    export LESS_TERMCAP_so=$(printf '\e[01;33m')
+    export LESS_TERMCAP_ue=$(printf '\e[0m')
+    export LESS_TERMCAP_us=$(printf '\e[04;36m')
+    export LS_COLORS="${LS_COLORS}:di=1;91:"
+    export TERM="xterm-256color"
 
-  # Git
-  export EDITOR=nano
-  export GIT_LFS_SKIP_SMUDGE=1
-  export GIT_TERMINAL_PROMPT=1
+    # Git
+    export EDITOR=nano
+    export GIT_LFS_SKIP_SMUDGE=1
+    export GIT_TERMINAL_PROMPT=1
 
-  # GPG
-  export GPG_TTY=$(tty)
+    # GPG
+    export GPG_TTY=$(tty)
 
-  # Golang
-  export GOROOT="${HOME}/.google/go"
-  export GOPATH="${GOROOT}/pkg"
-  export GOBIN="${GOROOT}/bin"
-  export PATH="${PATH}:${GOROOT}:${GOPATH}:${GOBIN}"
+    # Golang
+    export GOROOT="${HOME}/.google/go"
+    export GOPATH="${GOROOT}/pkg"
+    export GOBIN="${GOROOT}/bin"
+    export PATH="${PATH}:${GOROOT}:${GOPATH}:${GOBIN}"
 
-  # Gocryptfs
-  export PATH="${PATH}:${HOME}/.gocryptfs"
+    # Gocryptfs
+    export PATH="${PATH}:${HOME}/.gocryptfs"
 
-  # Perl5
-  export PERL5LIB="${HOME}/.perl5"
+    # Python
+    export _VIRTUALENV="${HOME}/.lib/pyradox3"
+    [ -d "$_VIRTUALENV" ] && {
+      . "${_VIRTUALENV}/bin/activate"
+    }
 
-  # Python
-  export _VIRTUALENV="${HOME}/.lib/pyradox3"
-  [ -d "$_VIRTUALENV" ] && . "${_VIRTUALENV}/bin/activate"
+    # Wine
+    export WINEPREFIX='/tmp/wine'
+  }
 
-  # tmpfs
-  [ -d /cache ] && (
+  # rcs
+  [ -f /etc/zsh_command_not_found ] && {
+    . /etc/zsh_command_not_found
+  }
+
+  # aliases
+  [ -f "${HOME}/.zsh_aliases" ] && {
+    . "${HOME}/.zsh_aliases"
+  }
+
+  # cache
+  [ -z /cache ] && (
     cd /cache
     mkdir -p firefox        # ~/.cache/mozilla
     mkdir -p google-chrome  # ~/.cache/google-chrome
     mkdir -p thumbnails     # ~/.cache/thumbnails
+    chmod 700 /cache/*
   )
-
-  # Wine
-  export WINEPREFIX='/tmp/wine'
 }
