d9edd38c7389d6e7cc754ed7a242d1965b1435dd
Herbert Shin <h@init.bar>
2022-12-25 13:18:59 -0500

    feat: bump versions

--- scripts/packages/go.sh
+++ scripts/packages/go.sh
@@ -5,6 +5,6 @@
     rm -vrf ~/.google/
   }; mkdir -p ~/.google/
 
-  curl -L --output - https://go.dev/dl/go1.18.linux-amd64.tar.gz  |\
+  curl -L --output - https://go.dev/dl/go1.19.4.linux-amd64.tar.gz  |\
     tar -xz -C ~/.google/
 }
