aaf778b5b55be02f9128962f2293c811495452d9
Herbert Shin <h@init.bar>
2022-12-26 12:06:31 -0500

    fix: dnscrypt config override

--- scripts/vm/home.jo/install.sh
+++ scripts/vm/home.jo/install.sh
@@ -22,8 +22,8 @@ function install_docker_ce() {
 
 function install_dnscrypt() {
   curl https://raw.githubusercontent.com/initbar/lib/main/scripts/dnscrypt-proxy.sh | bash
-  curl https://raw.githubusercontent.com/initbar/lib/main/scripts/vm/home.jo/config/dnscrypt-proxy.toml | tee dnscrypt-proxy.toml
   cd ~/.dnscrypt && {
+    curl https://raw.githubusercontent.com/initbar/lib/main/scripts/vm/home.jo/config/dnscrypt-proxy.toml | tee dnscrypt-proxy.toml
     sudo ./dnscrypt-proxy -service install
   }; cd -
 }
