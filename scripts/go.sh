245aa07d666709870be5b50ad02ffe012978c8b5
Herbert Shin <h@init.bar>
2024-08-13 22:17:32 -0400

    feat: bump go version

--- scripts/go.sh
+++ scripts/go.sh
@@ -5,6 +5,6 @@
     rm -vrf ~/.google/
   }; mkdir -p ~/.google/
 
-  curl -L --output - https://go.dev/dl/go1.20.5.linux-amd64.tar.gz  |\
+  curl -L --output - https://go.dev/dl/go1.23.0.linux-amd64.tar.gz  |\
     tar -xz -C ~/.google/
 }
