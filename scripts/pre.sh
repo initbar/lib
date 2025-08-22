2866865cea0d310201d895c520111ac8abdd2f76
Herbert Shin <h@init.bar>
2025-08-22 14:06:43 -0400

    rename package -> packages

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -3,10 +3,10 @@
 {
     # External packages
     packages=(
-        https://raw.githubusercontent.com/initbar/lib/main/scripts/dnscrypt-proxy.sh
-        https://raw.githubusercontent.com/initbar/lib/main/scripts/docker.sh
-        https://raw.githubusercontent.com/initbar/lib/main/scripts/git-lfs.sh
-        https://raw.githubusercontent.com/initbar/lib/main/scripts/go.sh
+        https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/dnscrypt-proxy.sh
+        https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/docker.sh
+        https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/git-lfs.sh
+        https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/go.sh
     ); for script in ${scripts[@]}; do curl "${script}" | bash; done
 
     # Directory fixes
