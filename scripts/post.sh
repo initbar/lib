749906b31bba9686e3f12938f4445a83c94943da
Herbert Shin <h@init.bar>
2025-08-22 16:59:48 -0400

    feat: add /dev/null ops

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 {
-    #
+    # Set git to SSH
     cd ~/.lib && {
         git remote set-url origin git@github.com:initbar/lib.git
     }
+
+    # /dev/null
+    ln -sf /dev/null ~/.bash_history
 }
