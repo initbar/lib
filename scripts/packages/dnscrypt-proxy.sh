9e8e1d02273d7e08dc0373efc4a8abb7b9b3550e
Herbert Shin <h@init.bar>
2025-08-30 21:11:38 -0400

    feat: dnscrypt init + setup

--- scripts/packages/dnscrypt-proxy.sh
+++ scripts/packages/dnscrypt-proxy.sh
@@ -2,18 +2,30 @@
 
 {
   [ -d ~/.dnscrypt ] && {
-    rm -vrf ~/.dnscrypt/
-  }; mkdir -v ~/.dnscrypt/
+    rm -vrf ~/.dnscrypt
+    mkdir -v ~/.dnscrypt
+  }
 
   curl -L --output - https://github.com/DNSCrypt/dnscrypt-proxy/releases/download/2.1.13/dnscrypt-proxy-linux_x86_64-2.1.13.tar.gz |\
     tar -xz -C ~/.dnscrypt
 
   cd ~/.dnscrypt/ && {
     mv -v linux-x86_64/* .
-    rm -vrf linux-x86_64/
+    rm -vrf linux-x86_64
   }
 
+  # DnsCrypt config
   [ -d ~/.lib ] && {
     ln -sf ~/.lib/internal/etc/dnscrypt-proxy.toml ~/.dnscrypt/dnscrypt-proxy.toml
+  }
+
+  # Start DnsCrypt
+  cd ~/.dnscrypt && {
+    # Stop previous instance
+    sudo systemctl stop dnscrypt-proxy.service
+
+    # Install new instance
+    sudo ./dnscrypt-proxy -service install
+    sudo ./dnscrypt-proxy -service start
   }
 }
