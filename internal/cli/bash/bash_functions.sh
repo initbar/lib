76c1ac3ebbde899ef39a5a2e37c2fbf5cf5c0822
Herbert Shin <h@init.bar>
2026-01-21 00:56:02 +0000

    feat: clean up

--- internal/cli/bash/bash_functions.sh
+++ internal/cli/bash/bash_functions.sh
@@ -1,93 +1,11 @@
 # ~/.bash_functions
 
 {
-  function __docker() {
-    if [ -n "$(docker network ls | grep wireguard)" ]; then
-      docker run \
-        --interactive \
-        --network "container:wireguard" \
-        --rm \
-        --tty \
-        --user "${UID}:${UID}" \
-        --volume "$(readlink -f ${PWD}):/sandbox" \
-        --workdir /sandbox \
-        "$@"
-    else
-      docker run \
-        --interactive \
-        --rm \
-        --tty \
-        --user "${UID}:${UID}" \
-        --volume "$(readlink -f ${PWD}):/sandbox" \
-        --workdir /sandbox \
-        "$@"
-    fi
-  }
-
-  function __docker_vpn() {
-    [ -n "$(docker network ls | grep wireguard)" ] || {
-      docker network create wireguard
-    }
-
-    docker run \
-      --cap-add=NET_ADMIN \
-      --cap-add=SYS_MODULE \
-      --detach \
-      --device /dev/net/tun \
-      --env PGID="$(id -g)" \
-      --env PUID="$(id -u)" \
-      --name wireguard \
-      --publish="127.0.0.1:${RANDOM}:46882/udp" \
-      --publish="127.0.0.1:${RANDOM}:50052/udp" \
-      --publish="127.0.0.1:${RANDOM}:51413/tcp" \
-      --publish="127.0.0.1:${RANDOM}:51413/udp" \
-      --publish="127.0.0.1:${RANDOM}:9091" \
-      --rm \
-      --sysctl net.ipv4.conf.all.src_valid_mark=1 \
-      --volume "$HOME/.vpn:/config" \
-      linuxserver/wireguard
-  }
-
-  function dsh() {
-    __docker initbar/lib:latest "$@"
-  }
-
-  function 7z() {
-    dsh \
-      7z "$@"
-  }
-
   function aptu() {
     sudo apt update
     sudo apt upgrade
     sudo apt autoremove
     sudo apt clean
-  }
-
-  function binwalk() {
-    dsh \
-      binwalk
-  }
-
-  function cal() {
-    dsh \
-      ncal
-  }
-
-  function ent() {
-    dsh \
-      ent \
-        -b \
-        "$@"
-  }
-
-  function et() {
-    dsh \
-      exiftool "$@"
-  }
-
-  function gip() {
-    curl https://ipinfo.io
   }
 
   function thumbnail() {
@@ -112,6 +30,84 @@
       mogrify -quality '60%' "$filename.png"
     } && rm ${seed}-*.png
   }
+}
+
+{
+  function __vpn_check() {
+    [ -n "$(docker network ls | grep wireguard)" ] || {
+      docker network create wireguard
+    }
+
+    [ -n "$(docker ps | grep vpn)" ] || {
+      # __vpn_openvpn
+      __vpn_wireguard
+    }
+  }
+
+  function __vpn_wireguard() {
+    docker run \
+      --cap-add=NET_ADMIN \
+      --cap-add=SYS_MODULE \
+      --detach \
+      --device /dev/net/tun \
+      --env PGID="$(id -g)" \
+      --env PUID="$(id -u)" \
+      --name vpn \
+      --publish="127.0.0.1:${RANDOM}:46882/udp" \
+      --publish="127.0.0.1:${RANDOM}:50052/udp" \
+      --publish="127.0.0.1:${RANDOM}:51413/tcp" \
+      --publish="127.0.0.1:${RANDOM}:51413/udp" \
+      --publish="127.0.0.1:${RANDOM}:9091" \
+      --rm \
+      --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+      --volume "$HOME/.vpn:/config" \
+      linuxserver/wireguard
+  }
+
+  function __docker() {
+    __vpn_check && {
+      docker run \
+        --interactive \
+        --network "container:vpn" \
+        --rm \
+        --tty \
+        --user "$(id -u):$(id -u)" \
+        --volume "$(readlink -f ${PWD}):/sandbox" \
+        --workdir /sandbox \
+        "$@"
+    }
+  }
+
+  function dsh() {
+    __docker initbar/lib:latest "$@"
+  }
+
+  function 7z() {
+    dsh \
+      7z "$@"
+  }
+
+  function binwalk() {
+    dsh \
+      binwalk
+  }
+
+  function cal() {
+    dsh \
+      ncal
+  }
+
+  function ent() {
+    dsh \
+      ent \
+        -b \
+        "$@"
+  }
+
+  function et() {
+    dsh \
+      exiftool "$@"
+  }
 
   function lrzip() {
     dsh \
@@ -122,18 +118,6 @@
     dsh \
       mat2 \
         --inplace \
-        "$@"
-  }
-
-  function mp3-dl() {
-    dsh \
-      yt-dlp \
-        --audio-format mp3 \
-        --audio-quality 320k \
-        --extract-audio \
-        --no-cache-dir \
-        --no-sponsorblock \
-        --verbose \
         "$@"
   }
 
@@ -149,20 +133,6 @@
         "$@"
   }
 
-  function rchk() {
-    dsh \
-      md5sum \
-        --binary \
-        --tag \
-        "$@"
-  }
-
-  function rsgn() {
-    local signature="$(md5sum $1 | head -c 8)"
-    local extension="$(echo $1 | tr . \\n | tail -n 1)"
-    mv -v "$1" "${signature}.${extension}"
-  }
-
   function strings() {
     dsh \
       strings \
@@ -170,50 +140,24 @@
         "$@"
   }
 
-  function skaffold() {
-    docker run \
-      gcr.io/k8s-skaffold/skaffold:latest \
-      skaffold \
-      "$@"
-  }
-
   function tcli() {
-    if [ -n "$(docker network ls | grep wireguard)" ]; then
-      __docker \
-        --detach \
-        --volume="${HOME}/.torrents:/home/ubuntu/Downloads" \
-        initbar/lib:latest \
-        transmission-cli \
-        --blocklist \
-        --encryption-preferred \
-        --no-downlimit \
-        --uplimit 1 \
-        --verify \
-        "$1"
-    else
-      __docker \
-        --detach \
-        --publish="127.0.0.1:${RANDOM}:46882/udp" \
-        --publish="127.0.0.1:${RANDOM}:50052/udp" \
-        --publish="127.0.0.1:${RANDOM}:51413/tcp" \
-        --publish="127.0.0.1:${RANDOM}:51413/udp" \
-        --publish="127.0.0.1:${RANDOM}:9091" \
-        --volume="${HOME}/.torrents:/home/ubuntu/Downloads" \
-        initbar/lib:latest \
-        transmission-cli \
-        --blocklist \
-        --encryption-preferred \
-        --no-downlimit \
-        --uplimit 1 \
-        --verify \
-        "$1"
-    fi
+    __docker \
+      --detach \
+      --volume="${HOME}/.torrents:/home/ubuntu/Downloads" \
+      initbar/lib:latest \
+      transmission-cli \
+      --blocklist \
+      --encryption-preferred \
+      --no-downlimit \
+      --uplimit 1 \
+      --verify \
+      "$1"
   }
 
   function tclis() {
     while :; do
       docker ps |\
-        egrep -v 'wireguard|CONTAINER' |\
+        egrep -v 'vpn|CONTAINER' |\
         awk '{print $1}' |\
         sort |\
         xargs -I{} sh -c 'PROGRESS=$(docker logs --tail 1 {} | egrep -o "(Progress: [0-9.]+%)|Seeding" | sort -u | tail -1 | sed "s/^/\[/; s/$/\]/"); echo "> Container: {} ${PROGRESS}"' &&\
@@ -250,6 +194,18 @@
         --no-cache-dir \
         --no-sponsorblock \
         --yes-playlist \
+        "$@"
+  }
+
+  function mp3-dl() {
+    dsh \
+      yt-dlp \
+        --audio-format mp3 \
+        --audio-quality 320k \
+        --extract-audio \
+        --no-cache-dir \
+        --no-sponsorblock \
+        --verbose \
         "$@"
   }
 }
