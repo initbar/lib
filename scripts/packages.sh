00316cb703e99a5f6ffb3830982a21350bbb7ba0
Herbert Shin <h@init.bar>
2026-08-02 00:55:05 +0000

    feat: update package scripts

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 {
-    packages=(
-        https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/dnscrypt-proxy.sh
-        https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/docker.sh
-        https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/git-lfs.sh
-        https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/go.sh
-    )
+  packages=(
+    https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/dnscrypt-proxy.sh
+    https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/docker.sh
+    https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/git-lfs.sh
+    https://raw.githubusercontent.com/initbar/lib/main/scripts/packages/go.sh
+  )
 
-    for package in ${packages[@]}
-        do curl "${package}" | bash
-    done
+  for package in ${packages[@]}; do
+    curl "${package}" | bash
+  done
 }
